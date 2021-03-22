7d48f80768f8cb69314d9d4874479719042e96ec
Dnyanesh Mahajan <55128545+dnyana02@users.noreply.github.com>
2021-03-22 22:17:56 +0530

    Update DrawingApp.java

--- First_Project/DrawingApp.java
+++ First_Project/DrawingApp.java
@@ -7,16 +7,6 @@ import org.springframework.core.io.FileSystemResource;
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//1: Basic method to implements obeject
-		
-		/* DrawTriangle dt=new DrawTriangle();
-		   dt.draw();
-        */
-		
-		// 2. By using Spring Factory for XML file
-		//    For which we need to create our own XML file
 		
 		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("Myxml.xml"));
 		DrawTriangle triangle=(DrawTriangle) factory.getBean("triangle");
