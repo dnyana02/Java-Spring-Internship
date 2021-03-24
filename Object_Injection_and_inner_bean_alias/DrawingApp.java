9e0be45057c101f7891c38aa102999b9df8b59c4
Dnyanesh Mahajan <55128545+dnyana02@users.noreply.github.com>
2021-03-24 20:13:04 +0530

    Create DrawingApp.java
    Application context and main method

---
+++ Object_Injection_and_inner_bean_alias/DrawingApp.java
@@ -0,0 +1,17 @@
+package org.demo.practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+	
+        
+		ApplicationContext context=new ClassPathXmlApplicationContext("Myxml.xml");
+		DrawTriangle triangle=(DrawTriangle) context.getBean("triangle");
+		triangle.draw();
+		
+	}
+
+}
